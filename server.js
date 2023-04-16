747fbfc03575b7c74aa60a1a3e5ee21e9293d2a2
Callum <38749032+mrcallumfleming@users.noreply.github.com>
2023-04-16 16:29:46 +0000

    final test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Successful response. 123');
+  res.send('Hello Bubz I love you!');
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
