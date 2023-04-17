e3a94d9a4901d2a62f8237c45a22b7ddcb340b0b
Callum Fleming <mrcallumfleming@gmail.com>
2023-04-17 13:06:47 +0200

    added mongo db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const MongoClient = require("mongodb");
 
 const app = express();
 
@@ -9,10 +10,21 @@ app.get('/', (req, res) => {
   return res.redirect("https://www.youtube.com/watch?v=8QxIIz1yEsA&t=41s");
 });
 
-app.post('/ingress', (req, res) => {
+app.post('/', (req, res) => {
   // console.log("req", req?.json);
   console.log("req", req.body);
+  console.log(req.db);
   return res.send("OK");
 })
+
+
+app.use(async(req, res, next) => {
+  const client = new MongoClient.MongoClient(
+    "mongodb://root:KJLfFjklkj5r*%26%5EslkjdfKJH23847@164.90.232.181:27017/?authMechanism=DEFAULT&authSource=admin"
+  );
+  await client.connect();
+  req.db = client.db("ioblast");
+  next();
+});
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
