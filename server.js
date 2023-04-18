0849caa7c0929284ea16890a060c0bcfd6ddf6b1
Callum Fleming <mrcallumfleming@gmail.com>
2023-04-18 10:33:42 +0200

    added event endpoint and device verification

--- server.js
+++ server.js
@@ -4,25 +4,39 @@ const MongoClient = require("mongodb");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// inject the database
 app.use(async(req, res, next) => {
   const client = new MongoClient.MongoClient(
     "mongodb://root:KJLfFjklkj5r*%26%5EslkjdfKJH23847@164.90.232.181:27017/?authMechanism=DEFAULT&authSource=admin"
   );
   await client.connect();
   req.db = client.db("ioblast");
-  console.log(req.headers);
+  next();
+});
+
+// device verification and authentication
+app.use(async(req, res, next) => {
+  const {auth, id, model} = req.headers;
+  const device = await req.db.collection("devices").findOne({
+    id, auth, model
+  });
+  if (!device) {
+    return res.status(403).end();
+  }
+  req.device = device;
   next();
 });
 
 app.get('/', (req, res) => {
-  // res.send('PLEASE MARRY ME!');
   return res.redirect("https://www.youtube.com/watch?v=8QxIIz1yEsA&t=41s");
 });
 
-app.post('/', (req, res) => {
-  // console.log("req", req?.json);
-  console.log("req", req.body);
-  // console.log(req.db);
+app.post('/event', (req, res) => {
+  console.log(req.device);
+  console.log(req.body);
+  console.log(req.headers);
   return res.send("OK");
 })
 
